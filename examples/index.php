7450f38e134b3892453aa7b84e3396385e923b59
Dragos Badea <bedeabza@gmail.com>
2011-03-25 13:28:00 +0200

    Impoved example

--- examples/index.php
+++ examples/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once('Bootstrap.php');
 
-$image = new \Bedeabza\Image(dirname(__FILE__).'/images/demo.jpg');
-$image->resize(200, 200, \Bedeabza\Image::RESIZE_TYPE_CROP);
-$image->watermark(dirname(__FILE__).'/images/watermark.png', \Bedeabza\Image::WM_POS_CENTER);
+use \Bedeabza\Image;
+
+$image = new Image(dirname(__FILE__).'/images/demo.jpg');
+$image->resize(200, 200, Image::RESIZE_TYPE_CROP);
+$image->watermark(dirname(__FILE__).'/images/watermark.png', Image::WM_POS_CENTER);
 $image->render();
