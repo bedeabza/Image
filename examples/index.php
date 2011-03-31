3a86212e338f142fdaeb57083648860a7762fd0d
Dragos Badea Test <bedeabza+test@gmail.com>
2011-03-31 04:11:12 +0300

    added comments in examples index

--- examples/index.php
+++ examples/index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Main demo file
+ * 
+ * This example illustrates a resize procedure with auto cropping, as well as adding a watermark to the image.
+ * Then the image is rendered
+ */
 require_once('Bootstrap.php');
 
 $image = new \Bedeabza\Image(dirname(__FILE__).'/images/demo.jpg');
